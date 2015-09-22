7b65a7b532e0a7e09493bcf842cb8b7262b1bd82
whiteSkar <sypower07@hotmail.com>
2015-09-21 23:11:36 -0700

    Finished lpthw tutorial

--- bin/app.py
+++ bin/app.py
@@ -6,15 +6,20 @@ urls = (
 
 app = web.application(urls, globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base="layout")
 
 class Index(object):
     def GET(self):
         return render.hello_form()
 
     def POST(self):
-        form = web.input(name="Nobody", greet="Hello")
+        form = web.input(image={}, name="Nobody", greet="Hello")
         greeting = "%s, %s" % (form.greet, form.name)
+        
+        imageFile = open(form.image.filename, 'wb')
+        imageFile.write(form.image.value)
+        imageFile.close()
+       
         return render.index(greeting = greeting)
 
 if __name__ == "__main__":
